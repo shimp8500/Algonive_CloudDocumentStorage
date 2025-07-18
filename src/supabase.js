0f4ba628261531daf3c2cc2d02bae632c5594660
Shiv Kumar <skpathak0267@gmail.com>
2025-07-19 02:53:24 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mrgydkfteuduanauupok.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZ3lka2Z0ZXVkdWFuYXV1cG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NTI3MzQsImV4cCI6MjA2ODQyODczNH0.8slzpX0PvcPEXy8mqJZwJushmi9_kocGRt9-fot2aVk';
-
+const supabaseKey = 'API_KEY'; // input your API KEY
+ 
 export const supabase = createClient(supabaseUrl, supabaseKey);
